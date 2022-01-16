9adfc9070f57c1f04764ecfb1269e194160392e1
Hamza Tariq <hamzatrq116@gmail.com>
2022-01-16 05:39:58 +0500

    Correcting type

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ export default {
   dbConfig: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: Number.parseInt(process.env.DB_PORT),
   },
   log: {
     format: 'combined',
